d0089cc17f0e1f900c7b8fd86040ecb474b244dd
phaneendra <phani.manthena27@gmail.com>
2023-08-08 17:33:45 +0530

    updating ./store-sales-project-airflow

--- dags/store_DAG.py
+++ dags/store_DAG.py
@@ -1,6 +1,9 @@
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+from datacleaner import data_cleaner
 
 
 default_args = {
@@ -13,3 +16,10 @@ default_args = {
 dag=DAG('store_dag', default_args=default_args, schedule_interval='@daily', catchup=False)
 
 t1=BashOperator(task_id='check_file_exists', bash_command='shasum ~/store_files_airflow/raw_store_transactions.csv', retries=2, retry_delay=timedelta(seconds=15), dag=dag)
+
+t2=PythonOperator(taskid='Clean_raw_csv', python_callable=data_cleaner, dag=dag)
+
+
+
+
+t1 >> t2
