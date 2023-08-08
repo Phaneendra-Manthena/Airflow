e2c36d113ede7457a84389051f515c0d9d362817
phaneendra <phani.manthena27@gmail.com>
2023-08-08 17:40:45 +0530

    updating ./store-sales-project-airflow

--- dags/store_DAG.py
+++ dags/store_DAG.py
@@ -5,7 +5,6 @@ from airflow.operators.python_operator import PythonOperator
 
 from datacleaner import data_cleaner
 
-
 default_args = {
     'owner': 'Airflow',
     'start_date': datetime(2023, 8, 8),
@@ -13,13 +12,24 @@ default_args = {
     'retry_delay': timedelta(seconds=5)
 }
 
-dag=DAG('store_dag', default_args=default_args, schedule_interval='@daily', catchup=False)
+dag = DAG('store_dag', default_args=default_args, schedule_interval='@daily', catchup=False)
 
-t1=BashOperator(task_id='check_file_exists', bash_command='shasum ~/store_files_airflow/raw_store_transactions.csv', retries=2, retry_delay=timedelta(seconds=15), dag=dag)
+t1 = BashOperator(
+    task_id='check_file_exists',
+    bash_command='shasum ~/store_files_airflow/raw_store_transactions.csv',
+    retries=2,
+    retry_delay=timedelta(seconds=15),
+    dag=dag
+)
 
-t2 = PythonOperator(task_id='clean_raw_csv', python_callable=data_cleaner)
+def clean_data():
+    # Call your data cleaning function here
+    data_cleaner()
 
-
-
+t2 = PythonOperator(
+    task_id='clean_raw_csv',
+    python_callable=clean_data,  # Call the function you defined
+    dag=dag
+)
 
 t1 >> t2
