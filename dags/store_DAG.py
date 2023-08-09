4f415eaac18d6a068634ea23b00fa39cde200648
phaneendra <phani.manthena27@gmail.com>
2023-08-09 09:54:26 +0530

    updating ./store-sales-project-airflow

--- dags/store_DAG.py
+++ dags/store_DAG.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.mysql_operator import MySqlOperator
+from airflow.operators.email_operator import EmailOperator
 
 from datacleaner import data_cleaner
 
@@ -56,13 +57,6 @@ with DAG('store_dag', default_args=default_args, schedule_interval='@daily', tem
         dag=dag
     )
 
-    # Delete existing files before running the task
-    delete_files = BashOperator(
-        task_id='delete_existing_files',
-        bash_command='rm -f /store_files_mysql/location_wise_profit.csv /store_files_mysql/store_wise_profit.csv',
-        dag=dag
-    )
-
     t6 = BashOperator(
         task_id='move_file1',
         bash_command='cat ~/store_files_airflow/location_wise_profit.csv && mv ~/store_files_airflow/location_wise_profit.csv ~/store_files_airflow/location_wise_profit_%s.csv' % yesterday_date,
@@ -74,4 +68,15 @@ with DAG('store_dag', default_args=default_args, schedule_interval='@daily', tem
         dag=dag
     )
 
-    t1 >> t2 >> t3 >> t4 >> t5 >> delete_files >> [t6, t7]
+    t8 = EmailOperator(task_id='send_email',
+                       to='example@example.com',
+                       subject='Daily report generated',
+                       html_content=""" <h1>Congratulations! Your store reports are ready.</h1> """,
+                       files=['/usr/local/airflow/store_files_airflow/location_wise_profit_%s.csv' % yesterday_date, '/usr/local/airflow/store_files_airflow/store_wise_profit_%s.csv' % yesterday_date]
+                       )
+
+    t9 = BashOperator(
+        task_id='rename_raw',
+        bash_command='mv ~/store_files_airflow/raw_store_transactions.csv ~/store_files_airflow/raw_store_transactions_%s.csv' % yesterday_date
+    )
+    t1 >> t2 >> t3 >> t4 >> t5 >> [t6, t7] >> t8 >> t9
