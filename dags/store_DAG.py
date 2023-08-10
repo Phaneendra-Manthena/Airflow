b237e3bd2140f605b50f36a10742e6afe2f49d2a
phaneendra <phani.manthena27@gmail.com>
2023-08-10 16:43:43 +0530

    updating ./store-sales-project-airflow

--- dags/store_DAG.py
+++ dags/store_DAG.py
@@ -1,9 +1,9 @@
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.operators.bash_operator import BashOperator
-# from airflow.operators.python_operator import PythonOperator
-# from airflow.operators.mysql_operator import MySqlOperator
-# from airflow.operators.email_operator import EmailOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.mysql_operator import MySqlOperator
+from airflow.operators.email_operator import EmailOperator
 
 from datacleaner import data_cleaner
 
@@ -16,14 +16,14 @@ default_args = {
     'retry_delay': timedelta(seconds=5)
 }
 
-with DAG('store_dag',default_args=default_args,schedule_interval='@daily', template_searchpath=['/usr/local/airflow/sql_files'], catchup=True) as dag:
+with DAG('store_dag', default_args=default_args, schedule_interval='@daily', template_searchpath=['/usr/local/airflow/sql_files'], catchup=True) as dag:
 
-t1 = BashOperator(
-    task_id='check_file_exists',
-    bash_command='shasum ~/store_files_airflow/raw_store_transactions.csv',
-    retries=2,
-    retry_delay=timedelta(seconds=15)
-)
+    t1 = BashOperator(
+        task_id='check_file_exists',
+        bash_command='shasum ~/store_files_airflow/raw_store_transactions.csv',
+        retries=2,
+        retry_delay=timedelta(seconds=15)
+    )
 
     t2 = PythonOperator(task_id='clean_raw_csv', python_callable=data_cleaner)
 
@@ -37,7 +37,6 @@ t1 = BashOperator(
 
     t7 = BashOperator(task_id='move_file2', bash_command='cat ~/store_files_airflow/store_wise_profit.csv && mv ~/store_files_airflow/store_wise_profit.csv ~/store_files_airflow/store_wise_profit_%s.csv' % yesterday_date)
 
-
     t8 = EmailOperator(task_id='send_email',
                        to='phani.manthena27@gmail.com',
                        subject='Daily report generated',
@@ -47,4 +46,3 @@ t1 = BashOperator(
     t9 = BashOperator(task_id='rename_raw', bash_command='mv ~/store_files_airflow/raw_store_transactions.csv ~/store_files_airflow/raw_store_transactions_{0}.csv'.format(yesterday_date))
 
     t1 >> t2 >> t3 >> t4 >> t5 >> [t6, t7] >> t8 >> t9
-    
