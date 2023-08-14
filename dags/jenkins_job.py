2def9645ccf121bc2bbbebef81092bf72e0fe6b8
phaneendra <phani.manthena27@gmail.com>
2023-08-14 15:10:30 +0530

    files added

--- dags/jenkins_job.py
+++ dags/jenkins_job.py
@@ -2,14 +2,28 @@ from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime
 
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 8, 14),
+    'retries': 1,
+}
+
 dag = DAG(
-    dag_id="jenkins_dag",
-    schedule_interval="@daily",
-    start_date=datetime(2023, 8, 14),  # Define an appropriate start date
+    'trigger_jenkins_dag',
+    schedule_interval=None,
+    default_args=default_args,
+    catchup=False,
 )
 
-start_jenkins = BashOperator(
-    task_id="start_jenkins",
-    bash_command="/usr/bin/sudo systemctl start jenkins",
+jenkins_server_url = 'http://34.16.152.234:8080'  # Replace with your Jenkins server URL
+jenkins_username = 'Phani'
+jenkins_password = 'mjrr'
+job_name = 'gradle build'
+
+trigger_jenkins_task = BashOperator(
+    task_id='trigger_jenkins_task',
+    bash_command=f'curl -X POST -u {jenkins_username}:{jenkins_password} {jenkins_server_url}/job/{job_name}/build',
     dag=dag,
 )
+
+trigger_jenkins_task
