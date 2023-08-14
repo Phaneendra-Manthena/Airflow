e55c4d993a5078b5e774ed6814f80c0a56c7c12d
phaneendra <phani.manthena27@gmail.com>
2023-08-14 13:52:16 +0530

    files added

---
+++ dags/jenkins_job.py
@@ -0,0 +1,15 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+
+dag = DAG(
+    dag_id="jenkins_dag",
+    schedule_interval="@daily",
+    start_date=datetime(2023, 8, 14),  # Define an appropriate start date
+)
+
+start_jenkins = BashOperator(
+    task_id="start_jenkins",
+    bash_command="sudo systemctl start jenkins",
+    dag=dag,
+)
