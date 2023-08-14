9cac4fa64b14108cf123d095ed66ed566fe7bea6
phaneendra <phani.manthena27@gmail.com>
2023-08-14 15:46:00 +0530

    files added

--- dags/jenkins_job.py
+++ dags/jenkins_job.py
@@ -15,22 +15,16 @@ dag = DAG(
     catchup=False,
 )
 
-jenkins_server_url = '34.16.152.234:8080'  # Replace with your Jenkins server URL
+jenkins_server_url = 'http://34.16.152.234:8080'  # Replace with your Jenkins server URL
 jenkins_username = 'Phani'
 jenkins_password = 'mjrr'
 job_name = 'gradle'
+crumb = 'a598d99a95d9ff4c59d0bcd2906ce714bb5a779e767e0fb9f19a34d7c69a7a68'  # Replace with your crumb
 
 trigger_jenkins_task = BashOperator(
     task_id='trigger_jenkins_task',
-    bash_command=f'curl -X POST -u {jenkins_username}:{jenkins_password} {jenkins_server_url}/job/{job_name}/build',
+    bash_command=f'curl -X POST -u {jenkins_username}:{jenkins_password} -H "Jenkins-Crumb: {crumb}" {jenkins_server_url}/job/{job_name}/build',
     dag=dag,
 )
 
 trigger_jenkins_task
-
-
-# curl -X POST http://34.16.152.234:8080/job/gradle/build --user Phani:mjrr -H 'Jenkins-Crumb: 6b3a16e39caa9388642c5c5c309aa6215ce272ed590e4a6cc8d221e9e77b6f05'
-#
-#
-#
-# curl -v -X POST http://34.16.152.234:8080/job/gradle/buildWithParameters?param=value --user Phani:117084d48630cc0d6245f985b4bc711864
